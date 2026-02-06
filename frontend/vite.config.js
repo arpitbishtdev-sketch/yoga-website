9cbf035aca34b104da5d3e7aaf5120140939b71d
Arpit Bisht <161747201+ArpitBisht2302@users.noreply.github.com>
2026-02-06 21:28:38 +0530

    spa routing fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // 👈 THIS LINE FIXES RENDER SPA REFRESH
+  base: "./", // ✅ THIS IS THE MAGIC
 });
