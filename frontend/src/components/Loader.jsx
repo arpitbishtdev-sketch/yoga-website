2d63b01fa4683f9df7d91b2c8718bc58c865aa2a
Arpit Bisht <161747201+ArpitBisht2302@users.noreply.github.com>
2026-02-06 19:23:48 +0530

    aa

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -4,9 +4,7 @@ export default function Loader() {
   return (
     <div className="loader-screen">
       <div className="loader-box">
-        <h1 style={{ color: "#1cdb2c", letterSpacing: "6px" }}>
-          IRON PARADISE
-        </h1>
+        <h1>MAHADEVAM YOGA</h1>
         <div className="loader-line" />
       </div>
     </div>
